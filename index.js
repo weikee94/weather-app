a3a9ae210abc54fc7958834758196ab21a1fac4e
weikee <weikee070194@gmail.com>
2020-04-17 14:46:41 +0800

    update weather app

---
+++ index.js
@@ -0,0 +1,92 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const geocode = require("./src/utils/geocode");
+const forecast = require("./src/utils/forecast");
+
+const app = express();
+
+// define paths for express config
+const publicDirectoryPath = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname, "./templates/partials/");
+
+// setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup html file
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather APP",
+    name: "Wei",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Wei",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    msg: "Anything.",
+    name: "Wei",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "Please provide a address value",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          location: location,
+          forecast: forecastData,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("notfound", {
+    title: "Help article not found",
+    msg: "Help article not found",
+    name: "Wei",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("notfound", {
+    title: "Not found page",
+    msg: "Page not found",
+    name: "Wei",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("app running");
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,3 @@
-console.log("loading client js");
-
-fetch("http://localhost:3000/weather?address=!").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data);
-    }
-  });
-});
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const msgOne = document.querySelector("#message-one");
@@ -21,16 +9,14 @@ weatherForm.addEventListener("submit", (e) => {
 
   msgOne.textContent = "Loading ...";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          msgOne.textContent = data.error;
-        } else {
-          msgOne.textContent = data.location;
-          msgTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        msgOne.textContent = data.error;
+      } else {
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
